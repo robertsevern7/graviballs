d32394856348bf62d9df53d60daa1876cd1b3aaf
Rob and Bu <Rob and Bu@RobandBu-HP>
2011-10-30 18:12:27 -0700

    Correct level count and fixed button size

--- src/com/graviballs/menus/LevelAdapter.java
+++ src/com/graviballs/menus/LevelAdapter.java
@@ -57,8 +57,7 @@ public class LevelAdapter extends BaseAdapter {
 
     private String[] levels = {
             "1", "2", "3", "4", "5", "6",
-            "7", "8", "9", "10", "11", "12",
-            "13", "14", "15", "16", "17", "18"
+            "7", "8", "9", "10"
     };
 
 }
